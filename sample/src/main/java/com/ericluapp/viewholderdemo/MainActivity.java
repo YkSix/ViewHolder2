fd9a37979d587ae572b167605b1eab0dcc40c032
Eric Lu <mtlu@yahoo-inc.com>
2017-03-03 01:13:57 +0800

    Add MyViewHolder#loadUrl in sample app.

--- sample/src/main/java/com/ericluapp/viewholderdemo/MainActivity.java
+++ sample/src/main/java/com/ericluapp/viewholderdemo/MainActivity.java
@@ -14,6 +14,19 @@ import com.ericluapp.libs.ViewHolder;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String[] IMAGES = new String[] {
+            "https://lh6.googleusercontent.com/-55osAWw3x0Q/URquUtcFr5I/AAAAAAAAAbs/rWlj1RUKrYI/s1024/A%252520Photographer.jpg",
+            "https://lh4.googleusercontent.com/--dq8niRp7W4/URquVgmXvgI/AAAAAAAAAbs/-gnuLQfNnBA/s1024/A%252520Song%252520of%252520Ice%252520and%252520Fire.jpg",
+            "https://lh5.googleusercontent.com/-7qZeDtRKFKc/URquWZT1gOI/AAAAAAAAAbs/hqWgteyNXsg/s1024/Another%252520Rockaway%252520Sunset.jpg",
+            "https://lh3.googleusercontent.com/--L0Km39l5J8/URquXHGcdNI/AAAAAAAAAbs/3ZrSJNrSomQ/s1024/Antelope%252520Butte.jpg",
+            "https://lh6.googleusercontent.com/-8HO-4vIFnlw/URquZnsFgtI/AAAAAAAAAbs/WT8jViTF7vw/s1024/Antelope%252520Hallway.jpg",
+            "https://lh4.googleusercontent.com/-WIuWgVcU3Qw/URqubRVcj4I/AAAAAAAAAbs/YvbwgGjwdIQ/s1024/Antelope%252520Walls.jpg",
+            "https://lh6.googleusercontent.com/-UBmLbPELvoQ/URqucCdv0kI/AAAAAAAAAbs/IdNhr2VQoQs/s1024/Apre%2525CC%252580s%252520la%252520Pluie.jpg",
+            "https://lh3.googleusercontent.com/-s-AFpvgSeew/URquc6dF-JI/AAAAAAAAAbs/Mt3xNGRUd68/s1024/Backlit%252520Cloud.jpg",
+            "https://lh5.googleusercontent.com/-bvmif9a9YOQ/URquea3heHI/AAAAAAAAAbs/rcr6wyeQtAo/s1024/Bee%252520and%252520Flower.jpg",
+            "https://lh5.googleusercontent.com/-n7mdm7I7FGs/URqueT_BT-I/AAAAAAAAAbs/9MYmXlmpSAo/s1024/Bonzai%252520Rock%252520Sunset.jpg"
+    };
+
     private RecyclerView mRecyclerView;
 
     private MyAdapter mMyAdapter;
@@ -49,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(mMyAdapter);
     }
 
-    private static class MyAdapter extends RecyclerView.Adapter<ViewHolder> {
+    private static class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
         private OnRecyclerViewItemClickListener mListener;
 
@@ -58,16 +71,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new ViewHolder(R.layout.item_simple, parent, mListener);
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new MyViewHolder(R.layout.item_simple, parent, mListener);
         }
 
         @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            holder.setText(R.id.tv_index, position + "");
+        public void onBindViewHolder(MyViewHolder holder, int position) {
             holder.setText(R.id.btn_button, position + "");
             holder.setOnClickListener(R.id.btn_button, mListener);
-            holder.setImageResource(R.id.iv_image, R.mipmap.ic_launcher);
+            holder.loadUrl(R.id.iv_image, IMAGES[position % IMAGES.length]);
         }
 
         @Override
