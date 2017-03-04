132da6b76062c092c71498a58aa0de91103b4e14
Eric Lu <mtlu@yahoo-inc.com>
2017-03-04 22:00:11 +0800

    Add more comments in OnRecyclerViewItemClickListener.java

--- library/src/main/java/com/ericluapp/libs/OnRecyclerViewItemClickListener.java
+++ library/src/main/java/com/ericluapp/libs/OnRecyclerViewItemClickListener.java
@@ -3,8 +3,16 @@ package com.ericluapp.libs;
 import android.view.View;
 
 public interface OnRecyclerViewItemClickListener extends View.OnClickListener {
+    /**
+     * @param holder The ViewHolder of the RecyclerView item.
+     * @param position The position in which the item is clicked.
+     */
     void onItemClick(ViewHolder holder, int position);
 
+    /**
+     * Note that the ViewHolder can be got by <code>v.getTag(R.id.tag_viewholder)</code>
+     * or using SimpleOnRecyclerViewItemClickListener#getViewHolder
+     */
     @Override
     void onClick(View v);
 }
